b6c7a2e6c43616bf4de9df9ba4f82a981797c671
Flavio Kruger Bittencourt <flavio.bittenca@gmail.com>
2017-06-24 01:39:40 -0300

    Adds template for Tests with VIPER Module.

---
+++ VIPER/VIPER+Quick Tests.xctemplate/___FILEBASENAME___Spec.swift
@@ -0,0 +1,78 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright © ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+//  Expected outcomes info: https://github.com/Quick/Nimble
+//
+
+import Foundation
+import Quick
+import Nimble
+
+@testable import ___PROJECTNAME___
+class ___FILEBASENAMEASIDENTIFIER___PresenterSpec: QuickSpec {
+    
+    private class MockInteractor: ___FILEBASENAMEASIDENTIFIER___InteractorInputProtocol {
+        
+        //setup Asserts
+        
+        fileprivate var interactorOutput: ___FILEBASENAMEASIDENTIFIER___InteractorOutputProtocol?
+        
+    }
+    
+    private class MockView: ___FILEBASENAMEASIDENTIFIER___PresenterOutputProtocol {
+        
+        //setup Asserts
+        
+        var presenter: ___FILEBASENAMEASIDENTIFIER___PresenterInputProtocol?
+        
+    }
+    
+    private class MockRouter: ___FILEBASENAMEASIDENTIFIER___RouterProtocol {
+        
+        //setup Asserts
+        
+        weak var view: UIViewController?
+        
+        static func assembleModule() -> UIViewController {
+            return UIViewController()
+        }
+        
+    }
+    
+    override func spec() {
+        
+        describe("___FILEBASENAMEASIDENTIFIER___Presenter") {
+            
+            var presenter: ___FILEBASENAMEASIDENTIFIER___Presenter!
+            var interactor: MockInteractor!
+            var router: MockRouter!
+            var view: MockView!
+            
+            beforeSuite {
+                presenter = ___FILEBASENAMEASIDENTIFIER___Presenter()
+                interactor = MockInteractor()
+                router = MockRouter()
+                view = MockView()
+                
+                presenter.interactor = interactor
+                presenter.presenterOutput = view
+                presenter.router = router
+            }
+            
+            beforeEach {
+            }
+            
+            context("view did load") {
+                it(<#"should do something"#>) {
+                    presenter.viewDidLoad()
+                    //expect(<##>).to(<##>)
+                }
+            }
+            
+        }
+    }
+}
